1d1acf85bd9e5bc8ba4999421682eb80db5e5f50
Stoev <forgotten@mail.bg>
2022-08-16 17:39:29 +0300

    Api routing examples

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,6 +2,16 @@
 use \Pecee\SimpleRouter\SimpleRouter;
 use \App\Controllers\PetsController;
 
+//Rest API routes Example
+SimpleRouter::match(['get'],'/api/pets', [PetsController::class, 'getAllPets']);
+SimpleRouter::match(['get'],'/api/pets/{id}', [PetsController::class, 'getById']);
+SimpleRouter::match(['post'],'/api/pets', [PetsController::class, 'create']);
+SimpleRouter::match(['put'],'/api/pets/{id}', [PetsController::class, 'update']);
+SimpleRouter::match(['delete'],'/api/pets/{id}', [PetsController::class, 'delete']);
+
+//Simple API routes exaple
 SimpleRouter::match(['get'],'/api/pets', [PetsController::class, 'getAllPets']);
 SimpleRouter::match(['get'],'/api/pet/{id}', [PetsController::class, 'getById']);
-SimpleRouter::match(['post'],'/api/pet', [PetsController::class, 'create']);
+SimpleRouter::match(['post'],'/api/pet/create', [PetsController::class, 'create']);
+SimpleRouter::match(['put'],'/api/pet/update/{id}', [PetsController::class, 'update']);
+SimpleRouter::match(['delete'],'/api/pet/delete/{id}', [PetsController::class, 'delete']);
